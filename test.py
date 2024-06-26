16afbe390418b667010e4b123728d4f94f398763
Olivier Doucet <odoucet@oxeva.fr>
2024-06-26 17:22:44 +0200

    add tests

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 import unittest
+import os
 from OcrWordFeudBoard import OcrWordfeudBoard
+import cv2
 
 tests = [
+    # {
+    #     "image_path": "tests/screens/test1.jpg",
+    #     "rack": "GGORTIE",
+    #     "played_letters": {
+    #         'CHENUE', 'OUREZ', 'OUKAS', 'AWA'
+    #     }
+    # },
     {
-        "image_path": "tests/test1.jpg",
-        "rack": "GGORTIE",
+        "image_path": "tests/screens/test2.jpg",
+        "rack": "EJECSLN",
         "played_letters": {
-            'CHENUE', 'OUREZ', 'OUKAS', 'AWA'
+            'DROIT', 'TYPO', 'UEE', 'HUEZ', 'LGS', 'UNS', 'CRAME', 'FNIONS', 'LUI', 'HIT', 'BMBER', 'LSE', 'XONS', 'VDKA', 'DEWAR', 'MOQUIT', 'BUTINEES', 'EVIE'
         }
     }
 ]
@@ -14,20 +23,45 @@ tests = [
 class TestOcr(unittest.TestCase):
     def test_detect_rack_and_board(self):
         for test in tests:
+            ocr = OcrWordfeudBoard()
+
             image_path = test["image_path"]
-            ocr = OcrWordfeudBoard(image_path)
 
             rack, played_letters = ocr.detect_rack_and_board(image_path)
 
-            # compare
-            self.assertEqual(''.join(rack), test["rack"])
-
-            # played letters:
             played_letters_sorted = ''.join(sorted(played_letters)).strip()
-            expected_played_letters_sorted = ''.join(sorted(test["played_letters"]))
-            
+            expected_played_letters_sorted = ''.join(sorted(''.join(test["played_letters"]))).strip()
 
-            self.assertEqual(played_letters_sorted, expected_played_letters_sorted)
+            with self.subTest():
+
+                # compare
+                self.assertEqual(''.join(rack), test["rack"])
+
+                # played letters:
+                self.assertEqual(played_letters_sorted, expected_played_letters_sorted)
+
+    def test_squares(self):
+        # load available letters = files in tests/squares:
+
+        squares = []
+        for file in os.listdir("tests/squares"):
+            if file.endswith(".png"):
+                squares.append(file)
+
+        ocr = OcrWordfeudBoard()
+
+        for square in squares:
+            square = "tests/squares/" + square
+            img = cv2.imread(square)
+            if img is None:
+                raise ValueError("Image '"+img+"' not found or path is incorrect")
+
+            letter = ocr.ocr_tile(img, save_image=False, comments="")
+
+            with self.subTest():
+                # filename is ocr_X,X-<letter>.png -> extract letter
+                expected = square.split("-")[1].split(".")[0]
+                self.assertEqual(letter, expected, msg=f"{square}: extracted {letter} but should be {expected}")
 
 
 if __name__ == '__main__':
